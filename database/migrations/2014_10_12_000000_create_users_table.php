23f392011023efb01075451f4c82f25b74a1f9fd
yuta051214 <ffftrick123@gmail.com>
2022-11-12 09:10:27 +0900

    マイグレーション、リレーション

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('shop')->nullable();
             $table->string('name');
+            $table->text('introduction')->nullable();
+            $table->string('address')->nullable();
+            $table->string('image')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
